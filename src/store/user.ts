ffa03420a32b26691ff165bb2040a312c197072d
zwp <hello@zwp.cn>
2025-04-03 19:14:36 +0800

    远程请求后端api

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,18 +1,27 @@
 // initial state
 import { StoreOptions } from "vuex";
 import AccessEnum from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: AccessEnum.NOT_LOGIN,
     },
   }),
   actions: {
-    getLoginUser({ commit, state }, payload) {
-      commit("updateUser", payload);
+    async getLoginUser({ commit, state }, payload) {
+      // 从远程请求获取登录信息
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: AccessEnum.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
