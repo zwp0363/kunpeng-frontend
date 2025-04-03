ff05e2133d1d88e6b9b142de21361847d28310a7
zwp <hello@zwp.cn>
2025-04-03 19:19:23 +0800

    用户通用布局，登录注册页面开发

---
+++ src/views/user/UserRegisterView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="userRegisterView">
+    <h2 style="margin-bottom: 16px">用户注册</h2>
+    <a-form
+      style="max-width: 480px; margin: 0 auto"
+      label-align="left"
+      auto-label-width
+      :model="form"
+      @submit="handleSubmit"
+    >
+      <a-form-item field="userAccount" label="账号">
+        <a-input v-model="form.userAccount" placeholder="请输入账号" />
+      </a-form-item>
+      <a-form-item field="userPassword" tooltip="密码不少于8位" label="密码">
+        <a-input-password
+          v-model="form.userPassword"
+          placeholder="请输入密码"
+        />
+      </a-form-item>
+      <a-form-item field="checkPassword" tooltip="重复密码" label="重复密码">
+        <a-input-password
+          v-model="form.checkPassword"
+          placeholder="请再次输入密码"
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" style="width: 120px"
+          >注册
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+import { UserControllerService, UserRegisterRequest } from "../../../generated";
+import message from "@arco-design/web-vue/es/message";
+import { useRouter } from "vue-router";
+
+/**
+ * 表单信息
+ */
+const form = reactive({
+  userAccount: "",
+  userPassword: "",
+  checkPassword: "",
+} as UserRegisterRequest);
+
+const router = useRouter();
+
+/**
+ * 提交表单
+ */
+const handleSubmit = async () => {
+  const res = await UserControllerService.userRegisterUsingPost(form);
+  // 注册成功，跳转到登录页面
+  if (res.code === 0) {
+    router.push({
+      path: "/user/login",
+      replace: true,
+    });
+  } else {
+    message.error("注册失败，" + res.message);
+  }
+};
+</script>
