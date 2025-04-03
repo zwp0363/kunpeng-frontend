f27d284bb9c1d8a93c282095a890abf38aaa0eea
zwp <hello@zwp.cn>
2025-04-03 19:17:16 +0800

    全局权限管理优化，用户自动登录

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <BasicLayout />
+    <template v-if="route.path.startsWith('/user')">
+      <router-view />
+    </template>
+    <template v-else>
+      <BasicLayout />
+    </template>
   </div>
 </template>
 
@@ -10,10 +15,10 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
 import { onMounted } from "vue";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
 /**
  * 全局初始化函数，有全局单次调用的代码，都可以写到这里
  */
@@ -23,20 +28,5 @@ const doInit = () => {
 
 onMounted(() => {
   doInit();
-});
-
-const router = useRouter();
-const store = useStore();
-
-router.beforeEach((to, from, next) => {
-  // 仅管理员可见，判断当前用户是否有权限
-  if (to.meta.access === "canAdmin") {
-    if (store.state.user.loginUser?.role !== "admin") {
-      // 用户不是管理员，重定向到无权限页
-      next("/noAuth");
-      return;
-    }
-  }
-  next(); // 如果不是 "canAdmin" 路由，或者用户是管理员，则继续导航
 });
 </script>
---
+++ src/access/index.ts
@@ -0,0 +1,30 @@
+import router from "@/router";
+import store from "@/store";
+import AccessEnum from "@/access/accessEnum";
+import ACCESS_ENUM from "@/access/accessEnum";
+import checkAccess from "@/access/checkAccess";
+
+router.beforeEach(async (to, from, next) => {
+  console.log("登录用户信息", store.state.user.loginUser);
+  const loginUser = store.state.user.loginUser;
+  // 如果之前没登陆过，自动登录
+  if (!loginUser || !loginUser.userRole) {
+    // 加await是为了等用户登陆成功之后，再执行后续代码
+    await store.dispatch("user/getLoginUser");
+  }
+  const needAccess = (to.meta?.access as string) ?? AccessEnum.NOT_LOGIN;
+  // 要跳转的页面必须要登录
+  if (needAccess !== ACCESS_ENUM.NOT_LOGIN) {
+    // 如果没登陆，跳转到登陆页面
+    if (!loginUser || !loginUser.userRole) {
+      next(`/user/login?redirect=${to.fullPath}`); // 登录后跳转到原页面
+      return;
+    }
+    // 如果已经登录了，但是权限不足，那么跳转到无权限页面
+    if (!checkAccess(loginUser, needAccess)) {
+      next("/noAuth");
+      return;
+    }
+  }
+  next(); // 如果是不用登录可访问页面，或者用户是管理员，则继续导航
+});
